4fed260947915b68351d1429d770f0022c8dec3e
huckl3berry <130092532+huckl3berry@users.noreply.github.com>
2023-11-09 12:41:00 -0500

    Update communesetup.py

--- communesetup.py
+++ communesetup.py
@@ -16,7 +16,15 @@ class EnvironmentSetup:
             "sudo systemctl start docker",    # Start Docker service
             "sudo systemctl enable docker",   # Enable Docker service on boot
             "sudo apt install sshpass -y",    # Install sshpass
-            "sudo apt install ansible -y"     # Install Ansible
+            "sudo apt install ansible -y",    # Install Ansible
+            "sudo apt install nvidia-driver-525 -y",  # Install Nvidia Driver 525
+            "wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin",
+            "sudo mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600",
+            "wget https://developer.download.nvidia.com/compute/cuda/12.0.0/local_installers/cuda-repo-ubuntu2204-12-0-local_12.0.0-525.60.13-1_amd64.deb",
+            "sudo dpkg -i cuda-repo-ubuntu2204-12-0-local_12.0.0-525.60.13-1_amd64.deb",
+            "sudo cp /var/cuda-repo-ubuntu2204-12-0-local/cuda-*-keyring.gpg /usr/share/keyrings/",
+            "sudo apt-get update",
+            "sudo apt-get -y install cuda"
         ]
 
     def setup_environment(self):
@@ -30,3 +38,4 @@ class EnvironmentSetup:
 if __name__ == "__main__":
     setup = EnvironmentSetup()
     setup.setup_environment()
+
